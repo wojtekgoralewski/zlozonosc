d406eebf689150da0517e5f15f16e75004e4a70a
wojtekgoralewski <wojtek.goralewski1@gmail.com>
2026-03-18 11:53:29 +0100

    info

---
+++ info.py
@@ -0,0 +1,7 @@
+"""
+Kolos - transformacja wielomianowa z jednego problemu na drugi
+Kolos - napisz maszynę turinga ktora przesuwa się o ciag tasmy o jedno w prawo
+
+
+zadanie - napisać maszynę turinga dla 
+"""
