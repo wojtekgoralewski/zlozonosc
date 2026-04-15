2695f911f461889f770bb985353433d56202d433
wojtekgoralewski <wojtek.goralewski1@gmail.com>
2026-04-15 15:45:09 +0200

    git

--- zajecia6/hamilton_solver.py
+++ zajecia6/hamilton_solver.py
@@ -8,22 +8,18 @@ def solve_hamilton_path(n, graph):
     def dfs(current_node):
         visited.add(current_node)
         
-        # Jeśli odwiedziliśmy n wierzchołków, znaleźliśmy ścieżkę Hamiltona
         if len(visited) == n:
             visited.remove(current_node)
             return True
             
-        # Przejście do sąsiadów
         for neighbor in graph[current_node]:
             if neighbor not in visited:
                 if dfs(neighbor):
                     return True
                     
-        # Backtracking - zdejmujemy wierzchołek przy powrocie
         visited.remove(current_node)
         return False
 
-    # Ponieważ szukamy ścieżki (a nie cyklu), możemy zacząć z dowolnego wierzchołka
     for start_node in range(n):
         if dfs(start_node):
             return True
